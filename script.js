7911f92127b64d3ee07b6e949bbfbb2cc4244a32
NBXOFF <nbx-off@outlook.fr>
2026-01-09 11:11:02 +0100

    Added name inputs form

--- script.js
+++ script.js
@@ -21,6 +21,12 @@ const winCombos = [
 ];
 
 // DOM variables
+const nameFormTitle = document.querySelector("#nameFormTitle");
+const form = document.querySelector("form");
+const inputPlayer1 = document.querySelector("#inputPlayer1");
+const inputPlayer2 = document.querySelector("#inputPlayer2");
+const saveNameBtn = document.querySelector("#saveNameBtn");
+
 const frameContainer = document.querySelector(".frame-container");
 const frames = document.querySelectorAll(".frame");
 const newGameBtn = document.querySelector(".new-game-btn");
@@ -39,7 +45,8 @@ function checkWinner() {
   for (const combo of winCombos) {
     const [a, b, c] = combo;
     if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      winningPlayerText.textContent = "Player " + board[a] + " wins!";
+      winningPlayerText.textContent =
+        players[a].name + " (" + board[a] + ") " + " wins!";
       gameOver = true;
     }
   }
@@ -50,6 +57,27 @@ function checkWinner() {
 }
 
 // UI
+
+nameFormTitle.addEventListener("click", () => {
+  form.classList.toggle("hidden");
+  newGameBtn.classList.toggle("hidden");
+});
+
+saveNameBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (inputPlayer1.value.trim() !== "") {
+    players[0].name = inputPlayer1.value;
+  }
+
+  if (inputPlayer2.value.trim() !== "") {
+    players[1].name = inputPlayer2.value;
+  }
+
+  form.classList.toggle("hidden");
+  newGameBtn.classList.toggle("hidden");
+});
+
 frameContainer.addEventListener("click", (e) => {
   const index = Number(e.target.dataset.index);
 
